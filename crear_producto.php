5a8e8f89a49b606233113c13aa9dc5d0fe780fbf
Nicolas <nicolascasclew@gmail.com>
2023-10-27 12:09:18 +0100

    inputs

---
+++ crear_producto.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<form>
+
+    <div>
+        <label for="nombre">Nombre producto:</label>
+        <input type="text" name="nombre" id="nombre" placeholder="Nombre completo">
+    </div>
+    <div>
+        <label for="precio">Precio :</label>
+        <input type="text" name="precio" id="precio" placeholder="Precio en euros">
+    </div>
+    <div>
+        <label for="imagen">Imagen producto:</label>
+        <input type="text" name="imagen" id="imagen" placeholder="Enlace a imagen ">
+    </div>
+    <div>
+        <label for="categoria">Categoria:</label>
+        <input type="text" name="categoria" id="categoria" placeholder="Categoria general">
+    </div>
+</form>
