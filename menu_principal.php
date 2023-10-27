ca4e3f1ceeeeed41f9b386d28725175b055a5116
Nicolas <nicolascasclew@gmail.com>
2023-10-27 10:41:09 +0100

    menu

--- menu_principal.php
+++ menu_principal.php
@@ -1,2 +1,24 @@
-<?php
-echo "buenas tardes";
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tienda Online</title>
+</head>
+
+<body>
+    <h1>Opciones</h1>
+    <a href="#">Crear producto</a>
+    </br>
+    </br>
+    <a href="#">Consultar el listado de productos</a>
+    </br>
+    </br>
+    <a href="#">Modificar producto</a>
+    </br>
+    </br>
+    <a href="#">Eliminar producto</a>
+</body>
+
+</html>
