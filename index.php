4af662f362df079576d4bdc5c4b9721598c17576
gbp12 <gonzabonilla4896@outlook.es>
2023-11-07 12:37:39 +0000

    pa nico

--- index.php
+++ index.php
@@ -13,12 +13,6 @@
     </br>
     </br>
     <a href="./views/listProducts.php">Consultar el listado de productos</a>
-    </br>
-    </br>
-    <a href="#">Modificar producto</a>
-    </br>
-    </br>
-    <a href="#">Eliminar producto</a>
 </body>
 
 </html>
